a8b3fcb657b3edf09734369d030bdf9b9604476f
kurtvelasco <kurtfrancisgame5@gmail.com>
2023-11-16 21:21:34 +0800

    reinvent

--- accountCreation.php
+++ accountCreation.php
@@ -21,13 +21,13 @@ $error = "";
                         $sql = "INSERT INTO tblaccounts (userName, userPassword, userEmail)
                             VALUES('$username', '$hash', '$email')";
                         mysqli_query($conn, $sql);
-                        echo "Account Created";
+                        $error = "Account Created";
                     } else {
                         $error = "Username or email already exists";
                     }
                 }
             } else {
-                $error = "Password not the same";
+                $error = "Password does not match same";
             }
         }
 
@@ -38,27 +38,53 @@ $error = "";
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="accountCreation.css"/>
+    <link rel="stylesheet" type="text/css" href ="test.css"/>
+    <link rel="stylesheet" type="text/css" href ="login.css"/>
 </head>
 <body>
-    <div class="creatAccount">
-        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">      
-            <label for="error" id="errorLabel"><?php echo $error; ?></label>
+<div class="meme">
+<img class ="specter" src="https://webusstatic.yo-star.com/ark_us_web/assets/166859053424210735/240e6cc28639286399279a65a8fca6f1.png"> 
+    <div class="createAccount">
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">     
+            <div class="topbar-logo">
+                    <img alt="logo" src="https://raw.githubusercontent.com/Aceship/Arknight-Images/main/factions/logo_iberia.png">
+                <div>
+                    <span>IBERIA</span>
+                </div>        
+            </div>
+            <span for="error" id="errorLabel"><?php echo $error; ?></span>
             <label for="username">Username:</label>
             <input type="text" name="username" autocomplete="off" placeholder="Enter Username." required>
             <label for="password">Password:</label>
             <input type="password" name="password" autocomplete="off" placeholder="Enter Password." required>
             <label for="confirmPassword">Confirm Password:</label>
-            <input type="password" name="confirmPassword" autocomplete="off" placeholder="Enter Password." required>
+            <input type="password" name="confirmPassword" autocomplete="off" placeholder="Confirm password." required>
             <label for="email">Email:</label>
             <input type="email" name="email" autocomplete="off" placeholder="Enter Email." required>
             <input type="submit" value="Register">  
+                <div class="trademark">
+                    <span>Status : <?php echo $message; ?> </span>
+                </div>
         </form>
-        <div class="logo">
-            <img alt="logo" src="https://raw.githubusercontent.com/Aceship/Arknight-Images/main/factions/logo_iberia.png">
-            <div>
-                <span>IBERIA</span>
-            </div>        
-        </div>
     </div>
+</div>
+        
+    <nav class="sidebar-container">
+            <ul class="sidebar-list">    
+                <li class="sidebar-logo">                     
+                    <a href="#" class="sidebar-link logo">  
+                        <span>RHINE</span> 
+                        <svg viewBox="0 0 24 24" fill="white" height="2.9rem" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>github</title> <rect width="24" height="24" fill="none"></rect> <path d="M12,2A10,10,0,0,0,8.84,21.5c.5.08.66-.23.66-.5V19.31C6.73,19.91,6.14,18,6.14,18A2.69,2.69,0,0,0,5,16.5c-.91-.62.07-.6.07-.6a2.1,2.1,0,0,1,1.53,1,2.15,2.15,0,0,0,2.91.83,2.16,2.16,0,0,1,.63-1.34C8,16.17,5.62,15.31,5.62,11.5a3.87,3.87,0,0,1,1-2.71,3.58,3.58,0,0,1,.1-2.64s.84-.27,2.75,1a9.63,9.63,0,0,1,5,0c1.91-1.29,2.75-1,2.75-1a3.58,3.58,0,0,1,.1,2.64,3.87,3.87,0,0,1,1,2.71c0,3.82-2.34,4.66-4.57,4.91a2.39,2.39,0,0,1,.69,1.85V21c0,.27.16.59.67.5A10,10,0,0,0,12,2Z"></path> </g></svg>        
+                    </a>        
+                </li>  
+                <li class="sidebar-items">
+                    <a href="#" class="sidebar-link">
+                        <svg viewBox="0 0 24 24" fill="white" height="2.9rem" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>github</title> <rect width="24" height="24" fill="none"></rect> <path d="M12,2A10,10,0,0,0,8.84,21.5c.5.08.66-.23.66-.5V19.31C6.73,19.91,6.14,18,6.14,18A2.69,2.69,0,0,0,5,16.5c-.91-.62.07-.6.07-.6a2.1,2.1,0,0,1,1.53,1,2.15,2.15,0,0,0,2.91.83,2.16,2.16,0,0,1,.63-1.34C8,16.17,5.62,15.31,5.62,11.5a3.87,3.87,0,0,1,1-2.71,3.58,3.58,0,0,1,.1-2.64s.84-.27,2.75,1a9.63,9.63,0,0,1,5,0c1.91-1.29,2.75-1,2.75-1a3.58,3.58,0,0,1,.1,2.64,3.87,3.87,0,0,1,1,2.71c0,3.82-2.34,4.66-4.57,4.91a2.39,2.39,0,0,1,.69,1.85V21c0,.27.16.59.67.5A10,10,0,0,0,12,2Z"></path> </g></svg>
+                        <span class="link-text">Github</span>   
+                    </a>        
+                </li>                
+                          
+            </ul> 
+        </nav>
 </body>
 </html>
--- connectDatabase.php
+++ connectDatabase.php
@@ -1,10 +1,14 @@
 <?php
+
+use FTP\Connection;
+
     $db_server = "localhost";
     $db_user = "root";
     $db_pass = "";
     $db_name = "iberia_database";
     $conn ="";
     $error = "";
+    $status = true;
 
     try{
         $conn = mysqli_connect($db_server,
@@ -14,12 +18,24 @@
     }
     catch(mysqli_sql_exception){
         $error = "Cannot Connect sadge";
-    }
+        $status = false;
+    }  
+    class ConnectionStatus {
+        public $connected;
 
-    if($conn){
-        echo "You are connected<br>";
+        // __construct is similar to a constructor instead of class name
+        public function __construct(bool $var) {
+            $this->connected = $var;
+        }
+    
+        public function getMessage() {
+            if ($this->connected) {
+                return "You are connected";
+            } else {
+                return "Connection failed";
+            }
+        }
     }
-    else{
-        echo "{$error}<br>";
-    }
+    $connectionStatus = new ConnectionStatus($status);
+    $message = $connectionStatus->getMessage();
 ?>
